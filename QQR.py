7c8de0e15b9d760ccadfd144e16ac5cb3022791c
Pacidus <Pacidus@gmail.com>
2021-10-09 10:20:34 +0200

    1st commit with all done allready

---
+++ QQR.py
@@ -0,0 +1,46 @@
+import qrcode
+from qrcode.image.styledpil import StyledPilImage
+from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
+from qrcode.image.styles.colormasks import RadialGradiantColorMask
+
+qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+qr.add_data("https://discord.com/invite/uwAjHAG")
+img = qr.make_image(
+    image_factory=StyledPilImage,
+    embeded_image_path="./logoDiscord.png",
+    module_drawer=RoundedModuleDrawer(1),
+    color_mask=RadialGradiantColorMask(
+        back_color=(255, 255, 255),
+        center_color=(0, 0, 0),
+        edge_color=(88, 101, 242),
+    ),
+)
+img.save("QQRcode_Discord.png")
+
+qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+qr.add_data("https://www.facebook.com/curieosity.curieosity.3")
+img = qr.make_image(
+    image_factory=StyledPilImage,
+    embeded_image_path="./logoFacebook.png",
+    module_drawer=RoundedModuleDrawer(1),
+    color_mask=RadialGradiantColorMask(
+        back_color=(255, 255, 255),
+        center_color=(0, 0, 0),
+        edge_color=(59, 89, 152),
+    ),
+)
+img.save("QQRcode_Facebook.png")
+
+qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
+qr.add_data("https://www.instagram.com/curieosity.su/?hl=fr")
+img = qr.make_image(
+    image_factory=StyledPilImage,
+    embeded_image_path="./logoInstagram.png",
+    module_drawer=RoundedModuleDrawer(1),
+    color_mask=RadialGradiantColorMask(
+        back_color=(255, 255, 255),
+        center_color=(0, 0, 0),
+        edge_color=(178, 49, 185),
+    ),
+)
+img.save("QQRcode_Instagram.png")
